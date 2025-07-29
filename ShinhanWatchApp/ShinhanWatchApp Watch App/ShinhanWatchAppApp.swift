b1736cd66606c97f73cda70a438043037f19d9a6
dl-00-e8 <sso07012@gmail.com>
2025-07-30 00:20:48 +0900

    README 업데이트

---
+++ ShinhanWatchApp/ShinhanWatchApp Watch App/ShinhanWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShinhanWatchAppApp.swift
+//  ShinhanWatchApp Watch App
+//
+//  Created by 이정진 on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ShinhanWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShinhanWatchApp/ShinhanWatchApp Watch AppTests/ShinhanWatchApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShinhanWatchApp_Watch_AppTests.swift
+//  ShinhanWatchApp Watch AppTests
+//
+//  Created by 이정진 on 7/29/25.
+//
+
+import Testing
+@testable import ShinhanWatchApp_Watch_App
+
+struct ShinhanWatchApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
